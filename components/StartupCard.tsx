806d4e1078131a6b600c1992666f19f5360c6576
Stanzy-Developer <nnoupingnouping@gmail.com>
2024-12-24 20:28:34 +0100

    resolve solve vercel build errors

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -20,6 +20,9 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
     description,
   } = post;
 
+  const authorImage = author?.image || "/logo.png";
+  const authorName = author?.name || "Anonymous";
+
   return (
     <li className="startup-card group">
       <div className="flex-between">
@@ -33,7 +36,7 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
       <div className="flex-between mt-5 gap-5">
         <div className="flex-1">
           <Link href={`/user/${author?._id}`}>
-            <p className="text-16-medium line-clamp-1">{author?.name}</p>
+            <p className="text-16-medium line-clamp-1">{authorName}</p>
           </Link>
           <Link href={`/startup/${_id}`}>
             <h3 className="text-26-semibold line-clamp-1">{title}</h3>
@@ -41,8 +44,8 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
         </div>
         <Link href={`/user/${author?._id}`}>
           <Image
-            src={author?.image!}
-            alt={author?.name!}
+            src={authorImage}
+            alt={authorName}
             width={48}
             height={48}
             className="rounded-full"
